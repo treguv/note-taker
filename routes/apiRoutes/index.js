ae0122dcb204bcb76e99e922e7ea91641efb043d
treguv <vladislavtregubov00@gmail.com>
2020-11-28 20:21:42 -0800

    added ability to write new notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,14 +2,16 @@
 const path = require("path"); //file path
 const router = require("express").Router(); // get the router obj from express
 const fs = require("fs"); // for reading/writing data
+const { v4: uuidv4 } = require("uuid");
 // const { create } = require("domain");
 const { currentNotes } = require("../../db/db.json");
 //set up expected post request
 router.post("/notes", (req, res) => {
   // this has prefix api in the main server
   console.log(req.body);
-  createNewNote({ title: req.body.title, text: req.body.text }); // make new note with this object
-  res.json({ title: req.body.title, text: req.body.text });
+  let newNote = { title: req.body.title, text: req.body.text, id: uuidv4() };
+  createNewNote(newNote); // make new note with this object
+  res.json(newNote);
 });
 router.get("/notes", (req, res) => {
   console.log("Get recived to api/notes");
@@ -18,12 +20,12 @@ router.get("/notes", (req, res) => {
 
 //function for creating and writing new note into db.json
 function createNewNote(theNote) {
-  console.log("made new note", currentNotes);
   currentNotes.push(theNote); // Push the note onto the array
   fs.writeFileSync(
     path.join(__dirname, "../../db/db.json"), // set path
     JSON.stringify({ currentNotes: currentNotes })
   );
+  console.log("made new note", currentNotes);
 }
 //export router for use in server
 module.exports = router;
